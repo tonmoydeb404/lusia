50701b615736dfd95371b3531ab4fc38cee4b1b2
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-01-27 23:51:33 +0600

    Add Blog page and update Home page layout; include new PostCard usage and integrate icon library

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [{ hostname: "cdn.hashnode.com" }],
+    remotePatterns: [
+      { hostname: "cdn.hashnode.com" },
+      { hostname: "media.graphassets.com" },
+    ],
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,84 +1,39 @@
-import PostCard from "@/components/cards/post-card";
 import { Button } from "@/components/ui/button";
-import { LucideCode } from "lucide-react";
+import { SiGithub } from "@icons-pack/react-simple-icons";
+import { LucideFile } from "lucide-react";
+import Image from "next/image";
 
-export default function Home() {
+type Props = {};
+
+const HomePage = (props: Props) => {
   return (
     <>
-      <div className="container grid grid-cols-5 gap-4 mt-20">
-        <div className="col-span-3">
-          <PostCard />
+      <section className="container py-24">
+        <Image
+          src={"https://media.graphassets.com/Fn8oPxB9Sja74IDWxqtO"}
+          alt="Tonmoy Deb"
+          width={100}
+          height={100}
+          className="mb-7 rounded-full"
+        />
+        <h2 className="text-4xl font-bold mb-2">Hello, I&apos;m Jhon Doe</h2>
+        <p className="text-lg mb-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In rem,
+          veritatis placeat possimus saepe sunt corporis obcaecati vel illo
+          voluptatum veniam fugiat numquam eligendi tempore, dolorum asperiores
+          ut expedita voluptate?
+        </p>
+        <div className="flex items-center flex-wrap gap-2">
+          <Button className="rounded-full">
+            <LucideFile /> View Resume
+          </Button>
+          <Button className="rounded-full" variant={"outline"}>
+            <SiGithub /> Github
+          </Button>
         </div>
-        <div className="col-span-2 flex flex-col gap-4">
-          <PostCard variant="compact" />
-          <PostCard variant="compact" />
-        </div>
-      </div>
-      <div className="container grid grid-cols-2 gap-4 mt-40">
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-      </div>
-
-      <div className="container mt-40">
-        <h2 className="text-3xl font-bold text-center mb-10">Article Tags</h2>
-
-        <div className="flex items-center justify-center flex-wrap gap-2 max-w-lg mx-auto">
-          {[
-            "Coding",
-            "Developer",
-            "Journey",
-            "Javascript",
-            "React JS",
-            "HTML",
-            "TailwindCSS",
-          ].map((item) => (
-            <Button key={item} variant={"outline"}>
-              <LucideCode /> {item}
-            </Button>
-          ))}
-        </div>
-      </div>
-
-      <footer className="mt-52 container grid grid-cols-4 gap-4">
-        <div className="col-span-2 pr-16">
-          <h2 className="text-lg font-bold mb-2">Tonmoy Deb</h2>
-          <p className="text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-            repudiandae quas mollitia enim delectus aperiam dolorum! Itaque hic
-            optio quia.
-          </p>
-        </div>
-
-        <div>
-          <h3 className="text-sm font-bold mb-4 text-muted-foreground">
-            Pages
-          </h3>
-
-          <ul className="flex flex-col gap-2 text-sm">
-            <li>Home</li>
-            <li>About</li>
-            <li>Blog</li>
-            <li>Projects</li>
-          </ul>
-        </div>
-        <div>
-          <h3 className="text-sm font-bold mb-4 text-muted-foreground">
-            Resources
-          </h3>
-
-          <ul className="flex flex-col gap-2 text-sm">
-            <li>Home</li>
-            <li>About</li>
-            <li>Blog</li>
-            <li>Projects</li>
-          </ul>
-        </div>
-      </footer>
+      </section>
     </>
   );
-}
+};
+
+export default HomePage;
