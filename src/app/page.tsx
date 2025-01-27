bc0ced1b5d8b2d1ac5e7c3a9fb1d45d70f0e5863
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-01-27 15:52:15 +0600

    Add PostCard component and update Home page layout; configure remote image patterns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import PostCard from "@/components/cards/post-card";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="container grid grid-cols-5 gap-5 mt-20">
+        <div className="col-span-3">
+          <PostCard />
+        </div>
+        <div className="col-span-2 flex flex-col gap-5">
+          <PostCard variant="compact" />
+          <PostCard variant="compact" />
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/components/cards/post-card/index.tsx
@@ -0,0 +1,56 @@
+import { cn } from "@/lib/utils";
+import { LucideBook, LucideChartNoAxesCombined } from "lucide-react";
+import Image from "next/image";
+
+export type Variant = "default" | "compact";
+
+type Props = {
+  variant?: Variant;
+};
+
+const PostCard = (props: Props) => {
+  const { variant = "default" } = props;
+
+  return (
+    <article>
+      <div className="aspect-video relative border rounded-lg overflow-hidden">
+        <Image
+          alt="Post"
+          src={
+            "https://cdn.hashnode.com/res/hashnode/image/upload/v1737906448215/20864f37-2892-40c8-8dec-649357106b4c.webp?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp"
+          }
+          fill
+        />
+      </div>
+
+      <div
+        className={cn(
+          "px-4 py-4",
+          variant === "compact" ? "hidden" : undefined
+        )}
+      >
+        <h2 className="text-2xl font-black mb-1">
+          The Art of Adaptation in a Developer’s Life
+        </h2>
+        <p className="text-lg mb-4 text-muted-foreground">
+          Find out how developers can keep up with changing technology and
+          enhance their skills.
+        </p>
+
+        <div className="flex items-center gap-3">
+          <div className="inline-flex items-center gap-1 text-sm">
+            <LucideBook size={16} />
+            <span>3 min read</span>
+          </div>
+
+          <div className="inline-flex items-center gap-1 text-sm">
+            <LucideChartNoAxesCombined size={16} />
+            <span>5 views</span>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default PostCard;
