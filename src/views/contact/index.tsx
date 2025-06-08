73de6c814858573cd82b1d3d85d302222f64496b
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-06-08 21:36:03 +0600

    feat: create ContactView component with HeaderSection

---
+++ src/views/contact/index.tsx
@@ -0,0 +1,16 @@
+import HeaderSection from "@/components/sections/header-section";
+
+type Props = {};
+
+const ContactView = (props: Props) => {
+  return (
+    <>
+      <HeaderSection
+        title="Contact Me"
+        description="Let me know what's on your mind and I'll get back to you as soon as possible."
+      />
+    </>
+  );
+};
+
+export default ContactView;
