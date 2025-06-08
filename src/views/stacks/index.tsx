7d8490a47fd0e5e184d78f229a6aa375801da58f
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-06-08 21:31:53 +0600

    refactor: improve layout responsiveness and adjust padding in StacksView

--- src/views/stacks/index.tsx
+++ src/views/stacks/index.tsx
@@ -15,11 +15,11 @@ const StacksView = (props: Props) => {
       <section className="container space-y-5">
         {stacks.map((item) => (
           <LabelSection label={item.title} key={item.id}>
-            <section className="grid grid-cols-2 p-2 gap-2">
+            <section className="grid grid-cols-1 md:grid-cols-2 p-1 sm:p-2 gap-2">
               {item.items.map((item) => (
                 <article
                   key={item.id}
-                  className="flex items-start p-4 gap-x-4 hover:bg-accent rounded-xl duration-200"
+                  className="flex items-start p-3 gap-x-4 hover:bg-accent rounded-xl duration-200"
                 >
                   <div className="shrink-0 size-8 inline-flex items-center justify-center bg-accent rounded-md">
                     <img
