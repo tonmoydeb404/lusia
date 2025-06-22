6e7349e7699918a28c1e90acb443c1092faf9244
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-06-22 21:16:38 +0600

    feat: added social links in contact  page

--- src/views/contact/index.tsx
+++ src/views/contact/index.tsx
@@ -1,16 +1,46 @@
+import { getIcon } from "@/common/icons";
 import HeaderSection from "@/components/sections/header-section";
 import LabelSection from "@/components/sections/label-section";
+import { Badge } from "@/components/ui/badge";
+import { fetchProfile } from "@/services/cms/profile";
+import Link from "next/link";
 import ContactForm from "./contact-form";
 
 type Props = {};
 
-const ContactView = (props: Props) => {
+const ContactView = async (props: Props) => {
+  const profileRes = await fetchProfile();
+
   return (
     <>
       <HeaderSection
         title="Contact Me"
         description="Let me know what's on your mind and I'll get back to you as soon as possible."
-      />
+      >
+        <div className="flex items-center flex-wrap gap-1.5 mt-3">
+          {profileRes?.socials.map((item) => {
+            const Icon = getIcon(item.ref);
+
+            return (
+              <Badge
+                key={item.href}
+                asChild
+                variant={"secondary"}
+                className="text-sm [&_svg]:!size-4"
+              >
+                <Link
+                  href={item.href}
+                  target={item.newTab ? "_blank" : undefined}
+                  title={item.title}
+                >
+                  <Icon />
+                  {item.title}
+                </Link>
+              </Badge>
+            );
+          })}
+        </div>
+      </HeaderSection>
       <div className="container">
         <LabelSection label="Get In touch">
           <ContactForm />
--- src/views/home/hero-section.tsx
+++ src/views/home/hero-section.tsx
@@ -21,26 +21,59 @@ const HeroSection = async (props: Props) => {
       )}
       <h2 className="text-4xl font-bold mb-2">{page.title}</h2>
       <p className="text-lg mb-10">{page.description}</p>
-      <div className="flex items-center flex-wrap gap-2">
-        {profile?.callToActions?.map((item, index) => {
-          const Icon = getIcon(item.ref);
+      <div className="flex justify-between flex-col sm:flex-row sm:items-center gap-5">
+        <div className="flex items-center flex-wrap gap-2">
+          {profile?.callToActions?.map((item, index) => {
+            const Icon = getIcon(item.ref);
 
-          return (
-            <Button
-              className="rounded-full"
-              asChild
-              key={item.href}
-              variant={index === 0 ? "default" : "outline"}
-            >
-              <Link
-                href={item.href}
-                target={item.newTab ? "_blank" : undefined}
+            return (
+              <Button
+                className="rounded-full"
+                asChild
+                key={item.href}
+                variant={index === 0 ? "default" : "outline"}
               >
-                {item.ref && <Icon />} {item.title}
-              </Link>
-            </Button>
-          );
-        })}
+                <Link
+                  href={item.href}
+                  target={item.newTab ? "_blank" : undefined}
+                >
+                  {item.ref && <Icon />} {item.title}
+                </Link>
+              </Button>
+            );
+          })}
+        </div>
+
+        <div className="flex items-center justify-end flex-wrap gap-1.5">
+          {profile.socials.map((item) => {
+            if (
+              profile.callToActions.findIndex((cta) => cta.ref === item.ref) !==
+              -1
+            ) {
+              return null;
+            }
+
+            const Icon = getIcon(item.ref);
+
+            return (
+              <Button
+                key={item.href}
+                asChild
+                size={"icon"}
+                variant={"outline"}
+                className="[&_svg]:size-6"
+              >
+                <Link
+                  href={item.href}
+                  target={item.newTab ? "_blank" : undefined}
+                  title={item.title}
+                >
+                  <Icon />
+                </Link>
+              </Button>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
