9f58d0da35687f84d1b3d18f31e5ea765959b725
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-06-28 18:31:42 +0600

    feat: added hashnode api revalidation

--- environment.d.ts
+++ environment.d.ts
@@ -2,7 +2,10 @@ namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_BASE_URL: string;
     NEXT_PUBLIC_GITHUB_USERNAME: string;
+
     HASHNODE_URL: string;
+    HASHNODE_REVALIDATE_SECRET: string;
+
     CMS_ENDPOINT: string;
     CMS_PROFILE_ID: string;
     CMS_REVALIDATE_SECRET: string;
--- src/common/env-config.ts
+++ src/common/env-config.ts
@@ -1,11 +1,14 @@
 const envConfig = {
-  HASHNODE_URL: process.env.HASHNODE_URL,
   BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   GITHUB_USERNAME: process.env.NEXT_PUBLIC_GITHUB_USERNAME,
   CMS: {
     ENDPOINT: process.env.CMS_ENDPOINT,
     PROFILE_ID: process.env.CMS_PROFILE_ID,
     REVALIDATE_SECRET: process.env.CMS_REVALIDATE_SECRET,
+  },
+  HASHNODE: {
+    URL: process.env.HASHNODE_URL,
+    REVALIDATE_SECRET: process.env.HASHNODE_REVALIDATE_SECRET,
   },
 };
 
