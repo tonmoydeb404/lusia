ad3afba33593251ed1d33d61ab6b879bbc5588bf
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-06-28 13:28:31 +0600

    fix: meta seo merge

--- src/helpers/metadata.ts
+++ src/helpers/metadata.ts
@@ -6,37 +6,40 @@ export const metaSeoToMetadata = (
   metaSeo: TCMSMetaSEO = {},
   metadata: Metadata = {}
 ): Metadata => {
-  return deepMerge<Metadata, Metadata>(metadata, {
-    title: metaSeo?.title,
-    description: metaSeo?.description,
-    alternates: {
-      canonical: metaSeo?.canonicalUrl,
-    },
-    robots: {
-      index: metaSeo?.index,
-      follow: metaSeo?.follow,
-    },
-    openGraph: {
+  return deepMerge<Metadata, Metadata>(
+    {
       title: metaSeo?.title,
       description: metaSeo?.description,
-      images: metaSeo?.image
-        ? {
-            url: metaSeo.image.url,
-            width: metaSeo.image.width,
-            height: metaSeo.image.height,
-          }
-        : undefined,
+      alternates: {
+        canonical: metaSeo?.canonicalUrl,
+      },
+      robots: {
+        index: metaSeo?.index,
+        follow: metaSeo?.follow,
+      },
+      openGraph: {
+        title: metaSeo?.title,
+        description: metaSeo?.description,
+        images: metaSeo?.image
+          ? {
+              url: metaSeo.image.url,
+              width: metaSeo.image.width,
+              height: metaSeo.image.height,
+            }
+          : undefined,
+      },
+      twitter: {
+        title: metaSeo?.title,
+        description: metaSeo?.description,
+        images: metaSeo?.image
+          ? {
+              url: metaSeo.image.url,
+              width: metaSeo.image.width,
+              height: metaSeo.image.height,
+            }
+          : undefined,
+      },
     },
-    twitter: {
-      title: metaSeo?.title,
-      description: metaSeo?.description,
-      images: metaSeo?.image
-        ? {
-            url: metaSeo.image.url,
-            width: metaSeo.image.width,
-            height: metaSeo.image.height,
-          }
-        : undefined,
-    },
-  });
+    metadata
+  );
 };
