3cf2011d6aa1bb95c34ab42d9f1a8605d178c443
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-11-06 17:13:42 +0600

    fix: hydration warning

--- src/layout/theme-btn.tsx
+++ src/layout/theme-btn.tsx
@@ -3,18 +3,34 @@
 import { Button } from "@/components/ui/button";
 import { useTheme } from "@/lib/next-themes";
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
 import { LuMoon, LuSun } from "react-icons/lu";
 
 type Props = {};
 
 const ThemeBtn = (props: Props) => {
   const { setTheme, theme, systemTheme, ref } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
   const appTheme = theme === "system" ? systemTheme : theme;
   const isDark = appTheme === "dark";
 
   const toggleTheme = () => {
     setTheme(isDark ? "light" : "dark");
   };
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted)
+    return (
+      <Button
+        size={"icon"}
+        variant={"secondary"}
+        className="animate-pulse"
+      ></Button>
+    );
 
   return (
     <Button
